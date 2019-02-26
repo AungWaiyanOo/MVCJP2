78897acda951f0bc4df6c1ed2ddab97e0851c460
AungWaiyanOo <aungwaiyanoo19@gmail.com>
2019-02-26 12:09:52 +0630

    first commit

---
+++ JackPot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JackPot.MvcApplication" Language="C#" %>
---
+++ JackPot/Models/FBUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace userlogin.Models
+{
+    public class FBUser
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Pictureurl { get; set; }
+        public string Email { get; set; }
+
+    }
+}
